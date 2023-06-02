b430abf36ab156f4bba6b6ee2e8838e6207e73c1
Sbhss <shankarpulaparthi1216@gmail.com>
2023-06-02 19:28:42 +0530

    done

---
+++ app.js
@@ -0,0 +1,36 @@
+const billAmount = document.querySelector("#billAmount");
+const cashGiven = document.querySelector("#cashGiven");
+const buttonCheck = document.querySelector("#buttonCheck");
+const errorMessage = document.querySelector("#errorMessage");
+const noOfNotes = document.querySelectorAll(".noOfNotes");
+
+const availableNotes = [500, 200, 100, 50, 20, 10, 5, 2, 1];
+
+buttonCheck.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if(billAmount > 0) {
+        if(cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value ;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("The cash provided should atleast be equal to the bill amount");
+        }
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    for(let i = 0; i<availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    };
+    amountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+}
+function hideMessage(){
+    errorMessage.style.display = "none";
+}
+function showMessage(msg){
+    errorMessage.style.display = "block";
+        errorMessage.innerText = msg;
+}
